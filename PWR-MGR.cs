70ba0d5115c78e629da2a2f888702176cacea44a
João Pedro <joaozinhovital2007@gmail.com>
2023-06-27 17:49:59 -0300

    Menu Iniciar reformulado para melhor estética com Spectre.Console

--- PWR-MGR.cs
+++ PWR-MGR.cs
@@ -7,23 +7,21 @@ using System.Net.Http.Headers;
 using System.Text;
 using Spectre.Console;
 
-namespace GerenciadordeSenhas
+namespace PWR.MGR
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            string mestra;
-            string senhaerrada;
-            senhaerrada = "A Senha Está Incorreta! Tente novamente.";
+            string mestra; //Variável que define a senha-mestra
 
+            //prompt inicial da senha mestra
             Console.WriteLine("Digite a Senha-Mestra");
-            mestra = Console.ReadLine();
-
+            mestra = Console.ReadLine(); 
             Console.Clear();
             
-            errado:
-            string tentativa;
+            verificar: //aqui acontece a verificação, se estiver errada, voltará pra cá
+            string tentativa; 
             Console.WriteLine("Digite a Senha-Mestra para continuar.");
             tentativa = Console.ReadLine();
             if (tentativa == mestra)
@@ -33,87 +31,55 @@ namespace GerenciadordeSenhas
             }
             else if (tentativa != mestra)
             {
-                Console.WriteLine(senhaerrada);
-                goto errado;
+                Console.WriteLine("A Senha Está Incorreta! Tente novamente.");
+                goto verificar;
             }
 
-        iniciar:
-            int menu;
-            bool um;
-            bool dois;
-            bool tres;
-            bool quatro;
-            um = false;
-            dois = false;
-            tres = false;
-            quatro = false;
-            
-            Console.ReadKey();
+            iniciar: //a partir daqui, o meu iniciar e suas opções...
             Console.Clear();
-            Console.WriteLine(@"================================
+            var mode = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title(@"================================
 GERENCIADOR DE SENHAS 2000 V0.01
-================================
-");
-            Console.WriteLine(@"Bem-vindo, este é o menu inicial. Selecione uma das opções abaixo:
- 1 - Ver suas senhas
- 2 - Gerenciar senhas
- 3 - Mudar Senha-Mestra
- 4 - Sair do Programa
-            ");
+================================")
+                    .AddChoices(new[]
+                        {
+                         "Ver as suas senhas", "Gerenciar senhas",
+                         "Mudar Senha-Mestra",  "Sair do Programa"
+                        }));
 
-            int.TryParse(Console.ReadLine(), out menu);
-
-            switch (menu)
+            if (mode == "Ver as suas senhas")
             {
-                case 1:
-                    {
-                        Console.Clear();
-                        Console.WriteLine("PLACEHOLDER");
-                        break;
-                    }
-                case 2:
-                    {
-                        Console.Clear();
-                        Console.WriteLine("PLACEHOLDER");
-                        break;
-                    }
-                case 3:
-                    {
-                        Console.Clear();
-                        tres = true;
-                        break;
-                    }
-                case 4:
-                    {
-                        Console.Clear();
-                        break;
-                    }
-                default:
-                    {
-                        Console.Clear();
-                        goto iniciar;
-                        break;
-                    }
+                Console.WriteLine("aaaaa funcionou");
             }
-
-            //pós menu:
-
-            if (tres = true)
+            else if (mode == "Gerenciar senhas")
             {
-                Console.Clear();
-                Console.WriteLine(@"Essa é a senha atual: {0}
+                Console.WriteLine("asijdauisdbauis");
+            }
+            else if (mode == "Mudar Senha-Mestra")
+            {
+                Console.WriteLine("Digite a Senha-Mestra para continuar.");
+                tentativa = Console.ReadLine();
+                if (tentativa == mestra)
+                {
+                    Console.Clear();
+                    Console.WriteLine(@"Essa é a senha-mestra atual: {0}
 Digite a nova senha.", mestra); //mostra a Mestra original
-                string novamestra;
-                novamestra = Console.ReadLine(); //input para a nova mestra
-
-                string rep1 = mestra.Replace($"{mestra}", novamestra);  //cria uma nova string substituindo o conteúdo de mestra por novamestra
-                Console.WriteLine("{0}", rep1);                         //retorna o 'novo' conteúdo 
-                //STATUS GERAL: "Works on my machine" 10/10!!!! //???????
-
+                    string novamestra;
+                    mestra = Console.ReadLine(); //input para a nova mestra
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("A Senha Está Incorreta! Tente novamente.");
+                    Console.ReadKey();
+                    goto iniciar;
+                }
             }
-
-
         }
     }
 }
 
+//EXCLUIR ISSO DEPOIS
+//string rep1 = mestra.Replace($"{mestra}", novamestra);  //cria uma nova string substituindo o conteúdo de mestra por novamestra
+//Console.WriteLine("{0}", rep1); //retorna o 'novo' conteúdo //STATUS GERAL: "Works on my machine" 10/10!!!! ??????
